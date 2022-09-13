965c974e133131ac3cf870bb20444105a9c43517
JakMcCarthy <90810973+JakMcCarthy@users.noreply.github.com>
2022-09-12 23:29:39 -0400

    Use routes and middleawre

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -34,6 +34,14 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+//Get 
+resolvers.get('*', (req, res) => {
+  res.sendFile(path.join(_dirname, '../client/public/index.html'));
+});
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 API listening on localhost:${PORT}!`);
+    console.log('Use GraphQL @ http://localhost:${PORT}$(server.graphqlPath}');
+  });
 });
