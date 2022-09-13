0eb997e552647dc9fe8c493cc58be063c998d420
JakMcCarthy <90810973+JakMcCarthy@users.noreply.github.com>
2022-09-12 23:19:25 -0400

    Input Apollo server and middlware

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const path = require('path');
+
+//import Apollo server 
+const{ ApolloServer } = require('apollo-server-express');
+//Import typeDefs and reesolvers
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+
+//db connection
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//express server
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context:authMiddleware
+});
+
+server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
