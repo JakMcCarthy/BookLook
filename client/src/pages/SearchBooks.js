b7b367c27a061775cd76fced098f9b66dde44c56
JakMcCarthy <90810973+JakMcCarthy@users.noreply.github.com>
2022-09-13 21:00:25 -0400

    Import save book and GET_ME

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -5,6 +5,9 @@ import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { useMutation } from '@apollo/client';
+import {SAVE_BOOK} from '../utils/mutation';
+import { GET_ME } from '../utils/queries';
+
 
 const SearchBooks = () => {
   // create state for holding returned google api data
